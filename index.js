05feaaa1149afe78a271db171a60a29c01673bf5
Matt Davis <matt_davis@me.com>
2016-10-10 00:48:58 -0500

    Updated with different address/port for testing, new this binding

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 var Alexa = require('alexa-sdk');
 var net = require('net');
+
 var testing = process.argv[2] || false; 
+var host = testing ? '192.168.0.6' : '64fc05cf370c.sn.mynetname.net';
+var port = testing ? 1255 : 32401;
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -18,26 +21,26 @@ var handlers = {
 		this.emit('PlayPause');
 	},
 	'PlayPause': function () {
-		var heos = net.connect({ host: '64fc05cf370c.sn.mynetname.net', port: 32401 }, function () {
+		var heos = net.connect({ host: host, port: port }, () => {
 			heos.write(`heos://player/get_play_state?pid=1699474811\r\n`, 'utf8', () => {
 				if (testing) console.log('Request sent');
 			});
 
-			heos.on('data', function (data) {
+			heos.on('data', data => {
 				var oldState = JSON.parse(data).heos.message.match(/state=(.*)/)[1];
 				var newState = oldState === 'pause' ? 'play' : 'pause';
 
 				heos.write(`heos://player/set_play_state?pid=1699474811&state=${newState}\r\n`, 'utf8', () => {
 					if (testing) console.log(`${oldState} changed to ${newState}`);
+					else this.emit(':tell', 'OK!');
+					
 					heos.end();
-					if (!testing) this.emit(':tell', 'OK!');
 				})
 			});
 		});
 	},
 	'AMAZON.HelpIntent': function () {
-		var speechOutput = "Just testing";
-		this.emit(':ask', speechOutput);
+		this.emit(':tell', 'Still testing!');
 	},
 	'AMAZON.CancelIntent': function () {
 		this.emit(':tell', 'Goodbye!');
