ce882149f68687e8972fd70933701196e4922f72
Matt Davis <matt_davis@me.com>
2016-10-09 22:58:20 -0500

    Revised play/pause 1.01

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const Alexa = require('alexa-sdk');
 const net = require('net');
 
@@ -9,13 +10,54 @@ exports.handler = function(event, context, callback) {
 };
 
 const handlers = {
-	'CondoPlay': () => setPlayState('play'),
-	'CondoPause': () => setPlayState('pause')
+	'LaunchRequest': function () {
+		this.emit('PlayPause');
+	},
+	'SetPlayState': function () {
+		this.emit('PlayPause');
+	},
+	'PlayPause': function () {
+		const heos = net.connect({ host: '192.168.0.6', port: 1255 }, () => {
+			heos.write(`heos://player/get_play_state?pid=1699474811\r\n`, 'utf8', () => {
+				console.log('Request sent');
+			});
+
+			heos.on('data', data => {
+				const oldState = JSON.parse(data).heos.message.match(/state=(.*)/)[1];
+				const newState = oldState === 'pause' ? 'play' : 'pause';
+
+				heos.write(`heos://player/set_play_state?pid=1699474811&state=${newState}\r\n`, 'utf8', () => {
+					console.log(`${oldState} changed to ${newState}`);
+					heos.end();
+				})
+			});
+		});
+	},
+	'AMAZON.HelpIntent': function () {
+		var speechOutput = "Just testing";
+		this.emit(':ask', speechOutput);
+	},
+	'AMAZON.CancelIntent': function () {
+		this.emit(':tell', 'Goodbye!');
+	},
+	'AMAZON.StopIntent': function () {
+		this.emit(':tell', 'Goodbye!');
+	}
 };
 
-function setPlayState (state='pause') {
+function playPause () {
 	const heos = net.connect({ host: '192.168.0.6', port: 1255 }, () => {
-		heos.write(`heos://player/set_play_state?pid=1699474811&state=${state}\r\n`);
-		heos.end();
+		heos.write(`heos://player/get_play_state?pid=1699474811\r\n`, 'utf8', () => {
+			console.log('Request sent');
+		});
+		heos.on('data', data => {
+			const oldState = JSON.parse(data).heos.message.match(/state=(.*)/)[1];
+			const newState = oldState === 'pause' ? 'play' : 'pause';
+
+			heos.write(`heos://player/set_play_state?pid=1699474811&state=${newState}\r\n`, 'utf8', () => {
+				console.log(`${oldState} changed to ${newState}`);
+				heos.end();
+			})
+		});
 	});
 }
